5ce1eb2debc1883b28673a56795da38fa5e2ff27
Alex Grover <hello@alexgrover.me>
2025-07-28 20:10:13 -0700

    Make button less wide

--- src/components/sign-in-button.tsx
+++ src/components/sign-in-button.tsx
@@ -16,7 +16,7 @@ export function SignInButton({ children = "Sign in" }: PropsWithChildren) {
         if (!connector) throw new Error("Missing connector");
         connect({ connector });
       }}
-      className="bg-blue-500 hover:bg-blue-600 text-white font-medium py-2 rounded-xl transition-colors duration-200 min-w-[200px] outline-1 outline-dashed outline-offset-2 outline-blue-400"
+      className="bg-blue-500 hover:bg-blue-600 text-white font-medium py-2 px-4 rounded-xl transition-colors duration-200 outline-1 outline-dashed outline-offset-2 outline-blue-400"
     >
       {children}
     </button>
